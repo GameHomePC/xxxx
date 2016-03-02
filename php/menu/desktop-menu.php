336ef1e0bacddedbc9383c51f3fd2b2b9e44800f
Yauhen Aliakseyenko <yauheni.aliakseyenko@neklo.com>
2016-03-02 22:41:56 +0300

    update

--- php/menu/desktop-menu.php
+++ php/menu/desktop-menu.php
@@ -10,36 +10,37 @@
                 <div class="wrapper">
                     <ul>
                         <li>
-                            <a href="/information.html">
-                                <span class="submenu__title">Подробная информация</span>
-                                <span class="submenu__description">Информация о ложе «Белый рыцарь», действующей в Беларуси.</span>
+                            <a href="/what.html">
+                                <span class="submenu__title">Что такое Масонство</span>
+                                <span class="submenu__description">Масонство – это свободное сообщество независимых людей безотносительно к их вероисповеданию, стремящееся к осуществлению идеалов мира, любви.</span>
                             </a>
                         </li>
 
                         <li>
-                            <a href="/news.html">
-                                <span class="submenu__title">Новости</span>
-                                <span class="submenu__description">Новости ложи «Белый рыцарь» Великой Ложи России</span>
+                            <a href="/history.html">
+                                <span class="submenu__title">История Масонства</span>
+                                <span class="submenu__description">Как любое значимое общественно-культурное явление, масонство России впитало в себя ее дух, ее многовековую историю, религию и культуру...</span>
                             </a>
                         </li>
 
                         <li>
-                            <a href="/zodcheskie.html">
-                                <span class="submenu__title">Зодческие</span>
-                                <span class="submenu__description"></span>
+                            <a href="/glossary.html">
+                                <span class="submenu__title">Словарь терминов</span>
+                                <span class="submenu__description">Словарь терминов, связанных с масонством</span>
                             </a>
                         </li>
 
                         <li>
-                            <a href="/library.html">
-                                <span class="submenu__title">Библиотека</span>
-                                <span class="submenu__description">Рекомендуемые к прочтению книги на тему Масонства</span>
+                            <a href="/principles.html">
+                                <span class="submenu__title">Цели и принципы</span>
+                                <span class="submenu__description">Наша миссия в этом мире – стать достойными гражданами, соблюдая высочайшие моральные принципы, важнейшими из которых являются...</span>
                             </a>
                         </li>
 
                         <li>
-                            <a href="/contact.html">
-                                <span class="submenu__title">Контакты</span>
+                            <a href="/main-characters.html">
+                                <span class="submenu__title">Главные символы</span>
+                                <span class="submenu__description">Главные масонские символы, знаки масонства</span>
                             </a>
                         </li>
                     </ul>
@@ -101,37 +102,36 @@
                 <div class="wrapper">
                     <ul>
                         <li>
-                            <a href="/what.html">
-                                <span class="submenu__title">Что такое Масонство</span>
-                                <span class="submenu__description">Масонство – это свободное сообщество независимых людей безотносительно к их вероисповеданию, стремящееся к осуществлению идеалов мира, любви.</span>
+                            <a href="/information.html">
+                                <span class="submenu__title">Подробная информация</span>
+                                <span class="submenu__description">Информация о ложе «Белый рыцарь», действующей в Беларуси.</span>
                             </a>
                         </li>
 
                         <li>
-                            <a href="/history.html">
-                                <span class="submenu__title">История Масонства</span>
-                                <span class="submenu__description">Как любое значимое общественно-культурное явление, масонство России впитало в себя ее дух, ее многовековую историю, религию и культуру...</span>
+                            <a href="/news.html">
+                                <span class="submenu__title">Новости</span>
+                                <span class="submenu__description">Новости ложи «Белый рыцарь» Великой Ложи России</span>
                             </a>
                         </li>
 
                         <li>
-                            <a href="/glossary.html">
-                                <span class="submenu__title">Словарь терминов</span>
-                                <span class="submenu__description">Словарь терминов, связанных с масонством</span>
+                            <a href="/zodcheskie.html">
+                                <span class="submenu__title">Зодческие</span>
+                                <span class="submenu__description"></span>
                             </a>
                         </li>
 
                         <li>
-                            <a href="/principles.html">
-                                <span class="submenu__title">Цели и принципы</span>
-                                <span class="submenu__description">Наша миссия в этом мире – стать достойными гражданами, соблюдая высочайшие моральные принципы, важнейшими из которых являются...</span>
+                            <a href="/library.html">
+                                <span class="submenu__title">Библиотека</span>
+                                <span class="submenu__description">Рекомендуемые к прочтению книги на тему Масонства</span>
                             </a>
                         </li>
 
                         <li>
-                            <a href="/main-characters.html">
-                                <span class="submenu__title">Главные символы</span>
-                                <span class="submenu__description">Главные масонские символы, знаки масонства</span>
+                            <a href="/contact.html">
+                                <span class="submenu__title">Контакты</span>
                             </a>
                         </li>
                     </ul>
--- php/menu/mobile-menu.php
+++ php/menu/mobile-menu.php
@@ -8,6 +8,74 @@
 
         <ul class="menuMobileSubmenu">
             <li>
+                <a href="/what.html" class="menuMobileSubmenu__title">Что такое Масонство</a>
+                <p>Масонство – это свободное сообщество независимых людей безотносительно к их вероисповеданию, стремящееся к осуществлению идеалов мира, любви.</p>
+            </li>
+
+            <li>
+                <a href="/history.html" class="menuMobileSubmenu__title">История Масонства</a>
+                <p>Как любое значимое общественно-культурное явление, масонство России впитало в себя ее дух, ее многовековую историю, религию и культуру...</p>
+            </li>
+
+            <li>
+                <a href="/glossary.html" class="menuMobileSubmenu__title">Словарь терминов</a>
+                <p>Словарь терминов, связанных с масонством</p>
+            </li>
+
+            <li>
+                <a href="/principles.html" class="menuMobileSubmenu__title">Цели и принципы</a>
+                <p>Наша миссия в этом мире – стать достойными гражданами, соблюдая высочайшие моральные принципы, важнейшими из которых являются...</p>
+            </li>
+
+            <li>
+                <a href="/main-characters.html" class="menuMobileSubmenu__title">Главные символы</a>
+                <p>Главные масонские символы, знаки масонства</p>
+            </li>
+        </ul>
+    </li>
+
+    <li class="menuMobile__item menuMobile__sub">
+        <a href="/">
+            ВЛР
+            <i class="submenu-icon"></i>
+        </a>
+
+        <ul class="menuMobileSubmenu">
+            <li>
+                <a href="/russian-freemasonry.html" class="menuMobileSubmenu__title">Российское Масонство</a>
+                <p>С самого начала своей истории в России, которое многие исследователи относят ко времени правления Петра I.</p>
+            </li>
+
+            <li>
+                <a href="/constitution.html" class="menuMobileSubmenu__title">Конституция</a>
+                <p>Принята Ассамблеей Великой Ложи 1 февраля 6003 года Истинного Света на Востоке г. Москвы</p>
+            </li>
+
+            <li>
+                <a href="/recognition.html" class="menuMobileSubmenu__title">Мировое признание</a>
+                <p>Взаимное признание (recognition) между Великими Ложами означает установление братских отношений, принятие на себя взаимных обязательств взаимопомощи.</p>
+            </li>
+
+            <li>
+                <a href="/register.html" class="menuMobileSubmenu__title">Реестр Лож</a>
+                <p>Полный список лож России</p>
+            </li>
+
+            <li>
+                <a href="/media-about.html" class="menuMobileSubmenu__title">СМИ о ВЛР</a>
+                <p>Средства массовой информации о Великой Ложе России</p>
+            </li>
+        </ul>
+    </li>
+
+    <li class="menuMobile__item menuMobile__sub">
+        <a href="/">
+            О ложе
+            <i class="submenu-icon"></i>
+        </a>
+
+        <ul class="menuMobileSubmenu">
+            <li>
                 <a href="/information.html" class="menuMobileSubmenu__title">Подробная информация</a>
                 <p>Информация о ложе «Белый рыцарь», действующей в Беларуси.</p>
             </li>
@@ -31,78 +99,6 @@
                 <a href="/contact.html" class="menuMobileSubmenu__title">Контакты</a>
             </li>
         </ul>
-    </li>
-
-    <li class="menuMobile__item menuMobile__sub">
-        <a href="/">
-            ВЛР
-            <i class="submenu-icon"></i>
-        </a>
-
-        <div class="menuMobileSubmenu">
-            <ul>
-                <li>
-                    <a href="/russian-freemasonry.html" class="menuMobileSubmenu__title">Российское Масонство</a>
-                    <p>С самого начала своей истории в России, которое многие исследователи относят ко времени правления Петра I.</p>
-                </li>
-
-                <li>
-                    <a href="/constitution.html" class="menuMobileSubmenu__title">Конституция</a>
-                    <p>Принята Ассамблеей Великой Ложи 1 февраля 6003 года Истинного Света на Востоке г. Москвы</p>
-                </li>
-
-                <li>
-                    <a href="/recognition.html" class="menuMobileSubmenu__title">Мировое признание</a>
-                    <p>Взаимное признание (recognition) между Великими Ложами означает установление братских отношений, принятие на себя взаимных обязательств взаимопомощи.</p>
-                </li>
-
-                <li>
-                    <a href="/register.html" class="menuMobileSubmenu__title">Реестр Лож</a>
-                    <p>Полный список лож России</p>
-                </li>
-
-                <li>
-                    <a href="/media-about.html" class="menuMobileSubmenu__title">СМИ о ВЛР</a>
-                    <p>Средства массовой информации о Великой Ложе России</p>
-                </li>
-            </ul>
-        </div>
-    </li>
-
-    <li class="menuMobile__item menuMobile__sub">
-        <a href="/">
-            О ложе
-            <i class="submenu-icon"></i>
-        </a>
-
-        <div class="menuMobileSubmenu">
-            <ul>
-                <li>
-                    <a href="/what.html" class="menuMobileSubmenu__title">Что такое Масонство</a>
-                    <p>Масонство – это свободное сообщество независимых людей безотносительно к их вероисповеданию, стремящееся к осуществлению идеалов мира, любви.</p>
-                </li>
-
-                <li>
-                    <a href="/history.html" class="menuMobileSubmenu__title">История Масонства</a>
-                    <p>Как любое значимое общественно-культурное явление, масонство России впитало в себя ее дух, ее многовековую историю, религию и культуру...</p>
-                </li>
-
-                <li>
-                    <a href="/glossary.html" class="menuMobileSubmenu__title">Словарь терминов</a>
-                    <p>Словарь терминов, связанных с масонством</p>
-                </li>
-
-                <li>
-                    <a href="/principles.html" class="menuMobileSubmenu__title">Цели и принципы</a>
-                    <p>Наша миссия в этом мире – стать достойными гражданами, соблюдая высочайшие моральные принципы, важнейшими из которых являются...</p>
-                </li>
-
-                <li>
-                    <a href="/main-characters.html" class="menuMobileSubmenu__title">Главные символы</a>
-                    <p>Главные масонские символы, знаки масонства</p>
-                </li>
-            </ul>
-        </div>
     </li>
 
     <li class="menuMobile__item"><a href="/questions.html">Вопросы</a></li>
