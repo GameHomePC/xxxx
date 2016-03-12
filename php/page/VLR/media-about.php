b7b707624d141f16b640e14025645a17f8473730
salexseen <salexseen@gmail.com>
2016-03-12 14:13:19 +0300

    update

--- php/page/VLR/media-about.php
+++ php/page/VLR/media-about.php
@@ -1,12 +1,17 @@
 <h1>СМИ о ВЛР</h1>
 
-<!--
-Использовать теги
+<p><a href="http://www.sovsekretno.ru/articles/id/4521/">Совершенно Секретно, Опубликовано: 19 Декабря 2014 16:17, No.32/327, А. Рыбалка, Лидер сицилийских масонов рассказал «Совершенно секретно» о том, откуда есть и пошла итальянская мафия</a></p>
+<p><a href="http://misanec.ru/2014/12/15/230-let-simbirskoj-masonskoj-lozhe/">230 лет Симбирской масонской ложе</a></p>
+<p><a href="http://gazeta.aif.ru/online/aif/1396/43_01">АИФ. Масонские тайны</a></p>
+<p><a href="http://vppress.ru/stories/V-Muzee-istorii-religii-otkroyut-tainy-masonstva-27268">Вечерний Петербург. В Музее истории религии откроют тайны масонства.</a></p>
+<p><a href="http://m.progorod76.ru/news/view/2269">Про Город. В Карабихе была своя масонская ложа.</a></p>
+<p><a href="http://www.ng.ru/ng_religii/2002-12-18/6_mason.html">Новая Газета. Масонами не рождаются.</a></p>
+<p>
+    <img src="images/kv_1.jpg" alt="kv_1">
+</p>
+<p>
+    <img src="images/kv_2.jpg" alt="kv_2">
+</p>
 
-Обзац:
-<p>Здесь текст</p>
 
-Изображение:
-<img src="" alt="Название картинки" >
--->
-<p>Здесь будет текст</p>
+
