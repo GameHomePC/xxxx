c337b64333401a90e473239a8f01d55e725e0912
salexseen <salexseen@gmail.com>
2016-03-16 00:50:59 +0300

    update

--- php/page/freemasonry/contact.php
+++ php/page/freemasonry/contact.php
@@ -1,4 +1,4 @@
 <h1>Контакты</h1>
 
 <p>Наш электронный адрес: <a href="mailto:mail@freemason.by">mail@freemason.by</a></p>
-<p>Наш форум: <a target="_blank" href="http://projectx.klochkoff.ru/forum/">projectx.klochkoff.ru</a></p>
+<p>Наш форум: <a target="_blank" href="/forum/">freemason.by</a></p>
